1e1c6ebda15feedcd3480b0631d2ade121ceb8de
TITAPISH <max-dadokin@mail.ru>
2024-07-02 20:26:59 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -6,5 +6,5 @@ print(immutable_var)
 #immutable_var [0] = 5 "Не поодерживает обращение по элементам"
 
 mutable_list = [1, 2, 'a', 'b']
-mutable_list = [1, 2, 'a', 'b','Modified']
+mutable_list.append('Modified')
 print(mutable_list)
