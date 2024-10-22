408331f057dafcb662e218cb21076eafa579cd81
TITAPISH <max-dadokin@mail.ru>
2024-10-22 19:09:28 +0300

    Update Homework19.py

--- Homework19.py
+++ Homework19.py
@@ -1,29 +1,10 @@
-def divide(first, second):
-    if second == 0:
-        return 'Ошибка'
-    else:
-        return first / second
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()
 
 
-import math
+test_function()
 
-def divide(first, second):
-    if second == 0:
-        return math.inf
-    else:
-        return first / second
-
-
-from fake_math import divide as fake_divide
-from true_math import divide as true_divide
-
-
-
-result1 = fake_divide(69, 3)
-result2 = fake_divide(3, 0)
-result3 = true_divide(49, 7)
-result4 = true_divide(15, 0)
-print(result1)
-print(result2)
-print(result3)
-print(result4)
+inner_function()
