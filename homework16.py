8ea94856df755e7b65043097cf16f2b9fe499026
TITAPISH <max-dadokin@mail.ru>
2024-11-08 19:28:49 +0300

    Update homework16.py

--- homework16.py
+++ homework16.py
@@ -2,9 +2,11 @@ def single_root_words(root_word, *other_words):
 
     same_words = []
 
+    root_word_lower = root_word.lower()
+
 
     for word in other_words:
-
+        word_lower = word.lower()
         if root_word.lower() in word.lower() or word.lower() in root_word.lower():
 
             same_words.append(word)
